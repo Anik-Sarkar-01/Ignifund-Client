f84f1d94e4b9d5b803629884cb367ff46ff2b8da
Anik-Sarkar-01 <aniksarkar8910@gmail.com>
2025-03-08 13:36:54 +0600

    React-simple-typewriter and React Awesome reveal packages added

--- src/components/CampaignCard/CampaignCard.jsx
+++ src/components/CampaignCard/CampaignCard.jsx
@@ -2,34 +2,37 @@ import React from 'react';
 import { MdAdd, MdOutlineUpdate } from 'react-icons/md';
 import { RiCoinsLine } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
+import { Zoom } from "react-awesome-reveal";
 
 const CampaignCard = ({ campaign }) => {
     const { _id, image, title, type, description, deadline, amount } = campaign;
     return (
-        <div className=" bg-base-100 w-96 shadow-sm">
-            <figure className='relative'>
-                <img
-                    src={image}
-                    alt="photo of ${title}" />
-                <div className='absolute text-center w-full bottom-0 bg-[#019DBF] px-2'>
-                    <div className="text-sm font-semibold"> Category: {type}</div>
-                </div>
-            </figure>
-            <div className="card-body space-y-2 border-2 border-[#019DBF] border-dotted">
-                <h2 className="card-title text-xl">
-                    {title}
-                </h2>
+        <Zoom>
+            <div className=" bg-base-100 w-96 shadow-sm">
+                <figure className='relative'>
+                    <img
+                        src={image}
+                        alt="photo of ${title}" />
+                    <div className='absolute text-center w-full bottom-0 bg-[#019DBF] px-2'>
+                        <div className="text-sm font-semibold"> Category: {type}</div>
+                    </div>
+                </figure>
+                <div className="card-body space-y-2 border-2 border-[#019DBF] border-dotted">
+                    <h2 className="card-title text-xl">
+                        {title}
+                    </h2>
 
-                <h3 className="text-[16px] flex items-center gap-1">  <MdOutlineUpdate className='text-[#019DBF]' />  <span className='font-semibold'>Deadline:</span> {deadline}</h3>
-                <div className="flex items-center gap-1 text-[16px]"> <RiCoinsLine className='text-[#019DBF]' />
-                    <span className='font-semibold'>Minimum Amount:</span> $ {amount}</div>
-                <p className='line-clamp-2'>{description}</p>
+                    <h3 className="text-[16px] flex items-center gap-1">  <MdOutlineUpdate className='text-[#019DBF]' />  <span className='font-semibold'>Deadline:</span> {deadline}</h3>
+                    <div className="flex items-center gap-1 text-[16px]"> <RiCoinsLine className='text-[#019DBF]' />
+                        <span className='font-semibold'>Minimum Amount:</span> $ {amount}</div>
+                    <p className='line-clamp-2'>{description}</p>
 
-                <div>
-                    <Link to={`campaign/${_id}`} className='btn text-[16px] bg-[#019DBF]'>See More <MdAdd className='text-[#019DBF]'></MdAdd></Link>
+                    <div>
+                        <Link to={`campaign/${_id}`} className='btn text-[16px] bg-[#019DBF]'>See More <MdAdd className='text-[#019DBF]'></MdAdd></Link>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Zoom>
     );
 };
 
--- src/components/SliderItem/SliderItem.jsx
+++ src/components/SliderItem/SliderItem.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { IoMdArrowForward } from 'react-icons/io';
+import { Link } from 'react-router-dom';
+import { Typewriter } from 'react-simple-typewriter';
 
 const SliderItem = ({ slide }) => {
     const { image, title, description } = slide;
@@ -11,20 +13,28 @@ const SliderItem = ({ slide }) => {
                     object-cover"
                     src={image}
                     alt={title}
-                    o
                 />
             </div>
 
             <div className="absolute inset-0 bg-gradient-to-r from-black/70 via-black/30 to-[#019DBF]/20 flex flex-col justify-center pl-10 space-y-5">
                 <h2 className="text-5xl font-extrabold text-white leading-relaxed">
-                    {title}
+                    <Typewriter
+                        words={[title]}
+                        loop={true}
+                        cursor
+                        cursorStyle="|"
+                        typeSpeed={70}
+                        cursorColor='#019DBF'
+                        deleteSpeed={50}
+                        delaySpeed={2000}
+                    />
                 </h2>
                 <p className="text-lg text-gray-200 italic max-w-xl leading-relaxed">
                     &ldquo;{description}&rdquo;
                 </p>
-                <button className="px-6 py-3 btn hover:bg-white w-fit bg-[#019DBF] rounded-none text-[16px] font-semibold">
+                <Link to="/allCampaigns" className="px-6 py-3 btn hover:bg-white w-fit bg-[#019DBF] rounded-none text-[16px] font-semibold">
                     Explore More <IoMdArrowForward />
-                </button>
+                </Link>
             </div>
         </div>
     );
